ab429082afcf40866224d81dd985fa8e4c4e2982
Joao Guilherme <marmore.besta@gmail.com>
2025-07-10 19:51:39 -0300

    subindo segunda versão do streamvibe no angular

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  filme1 = {
+    nome: "Titanic",
+    imagem: "/titanic.jpg",
+    sinopse: "Filme emocionante de um navio que afunda."
+  }
+
+  filme2 = {
+    nome: "Velocipastor",
+    imagem: "/velocipastor.jpg",
+    sinopse: "Filme emocionante de um pastor que vira velociraptor."
+  }
+
+  filme3 = {
+    nome: "Internet o filme",
+    imagem: "/Internet_O_Filme.jpg",
+    sinopse: "Filme emocionante e ruim de uns youtubers."
+  }
 }
