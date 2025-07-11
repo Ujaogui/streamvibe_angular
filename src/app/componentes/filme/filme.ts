5256db34df74427b9f49193b70a81a34cdf0e724
Joao Guilherme <marmore.besta@gmail.com>
2025-07-10 21:39:40 -0300

    subindo atualizações 10/07/25

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -8,21 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Filme {
 
+  alternarFavorito() {
+    this.filme1.favorito = !this.filme1.favorito
+  }
+
   filme1 = {
     nome: "Titanic",
     imagem: "/titanic.jpg",
-    sinopse: "Filme emocionante de um navio que afunda."
+    sinopse: "Filme emocionante de um navio que afunda.",
+    favorito: false
   }
 
-  filme2 = {
-    nome: "Velocipastor",
-    imagem: "/velocipastor.jpg",
-    sinopse: "Filme emocionante de um pastor que vira velociraptor."
-  }
+  // filme2 = {
+  //   nome: "Velocipastor",
+  //   imagem: "/velocipastor.jpg",
+  //   sinopse: "Filme emocionante de um pastor que vira velociraptor."
+  // }
 
-  filme3 = {
-    nome: "Internet o filme",
-    imagem: "/Internet_O_Filme.jpg",
-    sinopse: "Filme emocionante e ruim de uns youtubers."
-  }
+  // filme3 = {
+  //   nome: "Internet o filme",
+  //   imagem: "/Internet_O_Filme.jpg",
+  //   sinopse: "Filme emocionante e ruim de uns youtubers."
+  // }
 }
